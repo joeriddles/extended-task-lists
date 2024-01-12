1f064003c0b2b6e939dc16fdd62114964e1dde5d
joeriddles <joeriddles10@gmail.com>
2024-01-11 23:11:30 -0800

    Handle reading mode

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -16,71 +16,63 @@ export default class ExtendedMarkdownPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		const registerIndeterminateClick = (checkbox: HTMLInputElement) => {
+			let handled = false;
+			this.registerDomEvent(checkbox, 'click', (evt: MouseEvent) => {
+				if (handled) {
+					return;
+				}
+
+				handled = true;
+				checkbox.indeterminate = false;
+				checkbox.checked = true;
+				evt.stopPropagation();
+			});
+		}
+
+		const registerWontDoClick = (checkbox: HTMLInputElement, taskItem: HTMLElement) => {
+			let handled = false;
+			this.registerDomEvent(checkbox, 'click', (evt: MouseEvent) => {
+				if (handled) {
+					return;
+				}
+
+				handled = true;
+				checkbox.checked = true;
+				checkbox.classList.remove("wont-do");
+				taskItem.classList.remove("wont-do");
+				evt.stopPropagation();
+			});
+		}
+
+		this.registerMarkdownPostProcessor((element, context) => {
+			const taskItems = element.findAll(".task-list-item");
+			for (const taskItem of taskItems) {
+				const char = taskItem.dataset.task;
+				const checkbox = taskItem.find("input[type='checkbox']") as HTMLInputElement;
+
+
+				switch (char) {
+					case ".":
+						checkbox.indeterminate = true;
+						registerIndeterminateClick(checkbox);
+						break;
+					case "~":
+						checkbox.classList.add("wont-do");
+						taskItem.classList.add("wont-do");
+						registerWontDoClick(checkbox, taskItem);
+						break;
+				}
+			}
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
+	// onunload() {
 
-	}
+	// }
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -88,22 +80,6 @@ export default class ExtendedMarkdownPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 

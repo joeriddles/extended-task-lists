2cf0f9dac1c00a2f0761877455c48a12cc986344
joeriddles <joeriddles10@gmail.com>
2024-01-11 21:28:42 -0800

    Init project

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface ExtendedMarkdownSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: ExtendedMarkdownSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ExtendedMarkdownPlugin extends Plugin {
+	settings: ExtendedMarkdownSettings;
 
 	async onload() {
 		await this.loadSettings();
@@ -108,9 +108,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: ExtendedMarkdownPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ExtendedMarkdownPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}

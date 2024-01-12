d73038896a91ac844aa34a7ce190d3b33604f185
joeriddles <joeriddles10@gmail.com>
2024-01-11 23:38:45 -0800

    Cleanup and rename code

--- main.ts
+++ main.ts
@@ -1,23 +1,29 @@
-import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface ExtendedMarkdownSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: ExtendedMarkdownSettings = {
-	mySetting: 'default'
-}
-
-export default class ExtendedMarkdownPlugin extends Plugin {
-	settings: ExtendedMarkdownSettings;
+export default class ExtendedTaskListsPlugin extends Plugin {
 
 	async onload() {
-		await this.loadSettings();
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.registerMarkdownPostProcessor((element, context) => {
+			const taskItems = element.findAll(".task-list-item");
+			for (const taskItem of taskItems) {
+				const char = taskItem.dataset.task;
+				const checkbox = taskItem.find("input[type='checkbox']") as HTMLInputElement;
+
+
+				switch (char) {
+					case ".":
+						checkbox.indeterminate = true;
+						registerIndeterminateClick(checkbox);
+						break;
+					case "~":
+						checkbox.classList.add("wont-do");
+						taskItem.classList.add("wont-do");
+						registerWontDoClick(checkbox, taskItem);
+						break;
+				}
+			}
+		});
 
 		const registerIndeterminateClick = (checkbox: HTMLInputElement) => {
 			let handled = false;
@@ -47,64 +53,5 @@ export default class ExtendedMarkdownPlugin extends Plugin {
 				evt.stopPropagation();
 			});
 		}
-
-		this.registerMarkdownPostProcessor((element, context) => {
-			const taskItems = element.findAll(".task-list-item");
-			for (const taskItem of taskItems) {
-				const char = taskItem.dataset.task;
-				const checkbox = taskItem.find("input[type='checkbox']") as HTMLInputElement;
-
-
-				switch (char) {
-					case ".":
-						checkbox.indeterminate = true;
-						registerIndeterminateClick(checkbox);
-						break;
-					case "~":
-						checkbox.classList.add("wont-do");
-						taskItem.classList.add("wont-do");
-						registerWontDoClick(checkbox, taskItem);
-						break;
-				}
-			}
-		});
-	}
-
-	// onunload() {
-
-	// }
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: ExtendedMarkdownPlugin;
-
-	constructor(app: App, plugin: ExtendedMarkdownPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
